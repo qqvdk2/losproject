9aa017f65454cc8ada968b78d2ff5a105b4f56bf
Dell\qqvdk2 <1414674@hcmut.edu.vn>
2018-09-03 10:44:45 +0700

    First commit

---
+++ src/main/java/datavisian/lospredict/App.java
@@ -0,0 +1,13 @@
+package datavisian.lospredict;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
